89c917c397f74a3644cc0ecb245648f2e0c00878
Anthony Inope <inopeanthony@gmail.com>
2024-11-23 15:57:35 +0100

    code base

--- _main.tf
+++ _main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bootcampterraform"  
+    bucket         = "bootcampusers"  
     key            = "${terraform.workspace}/terraform.tfstate" 
     region         = "eu-west-1"  
     dynamodb_table = "terraform-lockss" 
